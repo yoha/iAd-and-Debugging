6b4181c9f0b6987408c91e21fb416547bac8b724
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-09-24 12:35:09 +0700

    Initial commit & push.

--- iAd and Debugging/ViewController.swift
+++ iAd and Debugging/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
 
+    // MARK: - Stored Properties
+    
+    var bannerView: ADBannerView!
+    
+    // MARK: - Methods Override
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.bannerView = ADBannerView(adType: .Banner)
+        self.bannerView.translatesAutoresizingMaskIntoConstraints = false
+        self.bannerView.delegate = self
+        self.bannerView.hidden = true
+        self.view.addSubview(self.bannerView)
+        
+        let viewsDictionary = ["bannerView": self.bannerView]
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Delegate Methods
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        self.bannerView.hidden = false
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        self.bannerView.hidden = true
+    }
 
 }
 
